34f0314df6acffd93207a08f3ae0662936ae824b
RomeroPablo <romeropablojr03@gmail.com>
2025-05-30 19:30:36 -0500

    init

--- server/dist_server.cpp
+++ server/dist_server.cpp
@@ -20,38 +20,64 @@ std::vector<int> clients;
 std::mutex client_lock;
 void client_addr(){
     // IPV4 & two way byte stream
-    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
     int opt = 1;
-    setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+    setsockopt(client_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
     sockaddr_in client_addr = {};
     client_addr.sin_family = AF_INET;
     client_addr.sin_addr.s_addr = INADDR_ANY;
     client_addr.sin_port = htons(CLIENT_PORT);
-    if(bind(client_fd, (sockaddr*)&client_addr, sizeof(client_addr))){
+    if(bind(client_socket, (sockaddr*)&client_addr, sizeof(client_addr))){
         perror("client bind");
     }
-    listen(client_fd, CLIENT_BACKLOG);
+    listen(client_socket, CLIENT_BACKLOG);
     std::cout << "[+] listening for clients on port " << CLIENT_PORT << "\n";
 
     // check for new connections, add them to a buffer
+    int clients_size = 0;
     while(true){
-        int
-
+        int client_fd = accept(client_socket, nullptr, nullptr);
+        if (client_fd < 0){
+            perror("client accept");
+            continue;
+        }
+        {
+            std::lock_guard lock(client_lock);
+            clients.push_back(client_fd);
+            clients_size = clients.size();
+        } 
+        std::cout << "[+] New client fd= " << client_fd << " (total: " << clients_size << ")\n";
     }
 }
 
 
 #define SOURCE_PORT 5600
 void source_init(){
+    int source_socket = socket(AF_INET, SOCK_STREAM, 0);
+    int opt_val = 1;
+    setsockopt(source_socket, SOL_SOCKET, SO_REUSEADDR, &opt_val, sizeof(opt_val));
+    sockaddr_in source_addr;
+    source_addr.sin_family = AF_INET;
+    source_addr.sin_addr.s_addr = INADDR_ANY;
+    source_addr.sin_port = htons(SOURCE_PORT);
+
+    if(bind(source_socket, (sockaddr*)&source_addr, sizeof(source_addr)) < 0){
+        perror("source bind");
+    }
+    listen(source_socket, 1);
+
+    std::cout << "[+] Waiting for source on port " << SOURCE_PORT << "...\n";
+    int source_fd = 0;
+    while(source_fd <= 0) {
+        sleep(1);
+        source_fd = accept(source_socket, nullptr, nullptr);
+    }
+    std::cout << "[+] Source connected, fd= " << source_fd <<"\n";
 
 }
 
 int main(void){
+    std::thread{client_addr}.detach();
 
+    source_init();
 }
-
-// fundamentally, what do we want?
-// we want a separate thread that listens for, and adds clients to our buffer of clients
-// once that is running, we should initialize our thread that sets up/looks for a source
-// once the source is connected, we distribute our information to all of our connected clients
-// if a client does not receive the message/disconencts, we remove it from our list
